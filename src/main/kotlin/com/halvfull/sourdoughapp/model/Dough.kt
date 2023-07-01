44b48473f44995ceb887d1f75dd9f1caa645b25a
Jakob <jakob.sterri@gmail.com>
2023-07-01 13:55:33 +0200

    added dough data class

---
+++ src/main/kotlin/com/halvfull/sourdoughapp/model/Dough.kt
@@ -0,0 +1,15 @@
+package com.halvfull.sourdoughapp.model
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+@Entity
+data class Dough(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0,
+    val hydration: Double,
+    val sourdoughGrams: Double,
+    val folds: Int,
+    val roomTempRisingTime: Double,
+    val coldRisingTime: Double)
+
