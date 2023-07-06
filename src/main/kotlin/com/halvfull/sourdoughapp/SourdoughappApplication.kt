2736ea82fce76b88880d9de4815a4f8cac97acf1
Jakob <jakob.sterri@gmail.com>
2023-07-06 15:13:43 +0200

    working api requests from front-end

--- src/main/kotlin/com/halvfull/sourdoughapp/SourdoughappApplication.kt
+++ src/main/kotlin/com/halvfull/sourdoughapp/SourdoughappApplication.kt
@@ -1,8 +1,10 @@
 package com.halvfull.sourdoughapp
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @SpringBootApplication
+@CrossOrigin(origins = ["http://localhost:3001"])
 class SourdoughApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/halvfull/sourdoughapp/controller/DoughController.kt
+++ src/main/kotlin/com/halvfull/sourdoughapp/controller/DoughController.kt
@@ -3,9 +3,13 @@ package com.halvfull.sourdoughapp.controller
 import com.halvfull.sourdoughapp.model.Dough
 import com.halvfull.sourdoughapp.service.DoughService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @RestController
 class DoughController(@Autowired val doughService: DoughService) {
@@ -14,7 +18,18 @@ class DoughController(@Autowired val doughService: DoughService) {
     fun addDough(@RequestBody dough: Dough): Dough {
         return doughService.save(dough)
     }
+
 }
+@Configuration
+class MyConfigurations {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedMethods("*")
+            }
+        }
+    }}
 
 
 
